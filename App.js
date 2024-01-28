4738d135930de8681edf2e819e8ea77126183239
Miraj Asraf <mirajasraf786@gmail.com>
2024-01-28 21:38:04 +0530

    Creating HTML-like nested structure using React

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
-const heading = React.createElement('h1', {id: "heading", miraj: "asraf"}, "Hello World from React!");
+// const heading = React.createElement('h1', {id: "heading", miraj: "asraf"}, "Hello World from React!");
 
-console.log(heading);
+/* Creating HTML-like nested structure */
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Miraj Asraf"),
+    React.createElement("p", {}, "Pursuing B.Tech from JGEC")
+  ])
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(parent);
 
-root.render(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
