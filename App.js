ea7f8e17761b2d5827a2e24b030d873ace485fa1
Miraj Asraf <mirajasraf786@gmail.com>
2024-02-08 20:39:31 +0530

    Explored features of Parcel, Browserslist, etc.

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-// const heading = React.createElement('h1', {id: "heading", miraj: "asraf"}, "Hello World from React!");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 /* Creating HTML-like nested structure */
 const parent = React.createElement(
@@ -6,11 +7,9 @@ const parent = React.createElement(
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "Miraj Asraf"),
-    React.createElement("p", {}, "Pursuing B.Tech from JGEC")
+    React.createElement("p", {}, "Pursuing B.Tech in Computer Science & Engineering from JGEC.")
   ])
 );
-
-console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
